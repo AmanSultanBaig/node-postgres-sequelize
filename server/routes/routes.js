eeb281800e6b0b2712fcbfbc79672152ceb14081
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-10-19 20:14:50 +0500

    changes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const studentController = require("../controllers/student.controller")
 
-router.get("/add_student", studentController.AddStudent)
+router.post("/add_student", studentController.AddStudent)
 router.get("/get_students", studentController.getStudents)
 
 module.exports = router
